f42244b8dec58e10ce4c0629f36fcead019955b9
Felipe Lourenco <felipe@countrhq.com>
2020-02-11 16:25:43 +0100

    Initial server setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const socket = require('socket.io')
+
+// App setup
+const app = express()
+const server = app.listen(4000, err => {
+  if (err) {
+    console.log('Something went wrong...')
+    return
+  }
+
+  console.log('Listening on port 4000...')
+})
+
+// Static files
+app.use(express.static('public'))
+
+// Socket setup
+const io = socket(server)
+
+io.on('connection', connectedSocket => {
+  console.log('TCL: connectedSocket', connectedSocket.id)
+  console.log('Socket connected!')
+})
